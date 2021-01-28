1823d4e3b8b6c8e4275cb60682e3367d3c8d8c90
Semyon <saymon221090@gmail.com>
2021-01-28 20:00:28 +0300

    создаёт модуль яндекс карт

---
+++ source/modules/yandexMapTracker.js
@@ -0,0 +1,104 @@
+class YandexMapTracker {
+  constructor() {
+    this._ymaps = window.ymaps;
+    this._myMap = null;
+    this._myPolyline = null;
+
+    this._points = [];
+    this._newPointId = 0;
+    this._activeMarkNumber = null;
+
+    this.addPointToMap = this.addPointToMap.bind(this);
+  }
+  // TODO: обеспечить обновление this._points при изменении порядка в списке, а так же дальшейний перерендер меток на карте
+  // * создать методы swapPoints , deletePoints
+
+  init() {
+    this._ymaps.ready(() => {
+      this._createMap();
+    })
+  }
+
+  addPointToMap(point) {
+    this._ymaps.geocode(point)
+      .then((response) => {
+        if (response.geoObjects.get(0)) {
+          const newMapMark = response.geoObjects.get(0);
+
+          const newPoint = {
+            id: this._newPointId,
+            name: newMapMark.properties.get("name"),
+            coordinates: newMapMark.geometry.getCoordinates(),
+          }
+
+          this._addPoint(newPoint);
+          this._addMarkToMap(newMapMark, this._newPointId);
+
+          this._newPointId++;
+
+          this._updatePolyline();
+        }
+      })
+  }
+
+  _createMap() {
+    this._myMap = new this._ymaps.Map("map", {
+      center: [55.76, 37.64],
+      zoom: 9,
+    })
+  }
+
+  _addPoint(pointData) {
+    this._points.push(pointData);
+  }
+
+  _addMarkToMap(mark, id) {
+    mark.properties.set("id", id);
+    mark.options.set("draggable", true);
+
+    mark.events.add("dragstart", () => {
+      this._activeMarkNumber = this._points.findIndex(({id}) => id === mark.properties.get("id"));
+    })
+
+    mark.events.add("drag", () => {
+      if (this._myPolyline) {
+        this._myPolyline.geometry.set(
+          this._activeMarkNumber,
+          mark.geometry.getCoordinates()
+        );
+      }
+    })
+
+    mark.events.add("dragend", () => {
+      const newCoordinates = mark.geometry.getCoordinates();
+      this._ymaps.geocode(newCoordinates)
+        .then((response) => {
+          const data = response.geoObjects.get(0).properties.getAll();
+          mark.properties.set(data);
+
+          this._points[this._activeMarkNumber].coordinates = mark.geometry.getCoordinates();
+          this._activeMarkNumber = null;
+        })
+    })
+
+    this._myMap.geoObjects.add(mark);
+  }
+
+  _updatePolyline() {
+    if (this._points.length > 2) {
+      const coordinates = this._points.map(({coordinates}) => coordinates);
+      this._myPolyline.geometry.setCoordinates(coordinates);
+    } else if (this._points.length > 1) {
+      this._createPolyline();
+    }
+  }
+
+  _createPolyline() {
+    const coordinates = this._points.map(({coordinates}) => coordinates);
+    this._myPolyline = new this._ymaps.Polyline(coordinates);
+
+    this._myMap.geoObjects.add(this._myPolyline);
+  }
+}
+
+export default new YandexMapTracker();
