1c2e1c0f66ea8f6f97e331b669092e365769d378
Semyon <saymon221090@gmail.com>
2021-02-01 22:20:27 +0300

    создаёт обработчики изменения данных

--- source/components/tracker/tracker.jsx
+++ source/components/tracker/tracker.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import DragList from "../drag-list/drag-list.jsx";
 import {useEffect, useState} from "react";
 import YandexMapTracker from "../../modules/yandexMapTracker";
+import {changeArrayElementsOrder} from "../../utils.js";
 
 export default function Tracker() {
   const [points, setPoints] = useState([]);
@@ -12,8 +13,42 @@ export default function Tracker() {
 
   function handleKeyDown(event) {
     if (event.key === "Enter") {
-      YandexMapTracker.addPointToMap(event.target.value);
+      YandexMapTracker.addPointToMap(event.target.value)
     }
+  }
+
+  function handlePointAdd(newPoint) {
+    setPoints((prev) => {
+      return [...prev, newPoint];
+    })
+  }
+
+  function handlePointDelete(pointToDelete) {
+    setPoints((prevPoints) => {
+      return prevPoints.filter((point) => point !== pointToDelete);
+    })
+
+    YandexMapTracker.deletePoint(pointToDelete);
+  }
+
+  function handlePointsOrderChange(nearbyPoint, movedPoint, pastePosition) {
+    setPoints((prev) => {
+      const currentPoints = prev.slice();
+      return changeArrayElementsOrder(currentPoints, nearbyPoint, movedPoint, pastePosition);
+    })
+
+    YandexMapTracker.changePointsOrder(nearbyPoint, movedPoint, pastePosition);
+  }
+
+  function handlePointDataChange(point, changedPoint) {
+    setPoints((prev) => {
+      const currentPoints = prev.slice();
+      const changedPointIndex = currentPoints.indexOf(point);
+
+      currentPoints[changedPointIndex] = changedPoint;
+
+      return currentPoints
+    });
   }
 
   return (
